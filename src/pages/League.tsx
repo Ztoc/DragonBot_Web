892ca1398ba68fcdd14783046c2de217b43cc2dc
mikiyas lemlemu <Mikiyaslemlemu@gmail.com>
2024-02-08 20:25:49 +0300

    League back button

--- src/pages/League.tsx
+++ src/pages/League.tsx
@@ -1,7 +1,13 @@
 import LeagueHeader from "../components/league/LeagueHeader.tsx";
 import '../league.css';
+import {useNavigate} from "react-router-dom";
+import WebApp from "@twa-dev/sdk";
 
 const League = () => {
+    const navigate = useNavigate();
+    WebApp.BackButton.onClick(() => navigate(-1))
+    WebApp.BackButton.show();
+    
     return (
         <div>
             <LeagueHeader />
