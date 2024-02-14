79da3704ee1e63a570ffb51781afbcfe7cfad9e5
mikiyas lemlemu <Mikiyaslemlemu@gmail.com>
2024-02-14 15:34:11 +0300

    Loading Bug Debug 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Terminal from 'vite-plugin-terminal'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    Terminal(),
     react()
   ]
 })
