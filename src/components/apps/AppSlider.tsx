f129b3c007362149f85a47df2b46e83c3a5adad4
Mohsen-T <mohsentbusiness@gmail.com>
2024-06-02 03:14:14 +0700

    Build bug fix

--- src/components/apps/AppSlider.tsx
+++ src/components/apps/AppSlider.tsx
@@ -1,33 +1,49 @@
 import { motion } from 'framer-motion';
-import { Page } from "./Page.tsx";
 import { useNavigate } from "react-router-dom";
 import WebApp from "@twa-dev/sdk";
+import { useSpringCarousel } from 'react-spring-carousel'
+import React from 'react';
 
-const images = [
-    '../public/background/dashboard.jpg',
-    '../public/background/dragon-war.jpg'
-];
-
+const slideItems = [{
+    id: 'app',
+    img: '../public/background/dashboard.jpg'
+},{
+    id: 'dragon-war',
+    img: '../public/background/dragon-war.jpg'
+}]
 const AppSlider = () => {
     const navigate = useNavigate();
-
+    const [currentSlide, setCurrentSlide] = React.useState(slideItems[0].id)
+    const { carouselFragment, useListenToCustomEvent, slideToPrevItem, slideToNextItem } = useSpringCarousel({
+        itemsPerSlide: 1, 
+        initialStartingPosition: 'center',
+        gutter: 0,
+        items: slideItems.map((item) => {
+            return {
+                ...item,
+                renderItem: (
+                    <div
+                        className={`slide w-full transition-all duration-700 ${currentSlide === item.id
+                            ? 'z-10'
+                            : 'scale-90'
+                            }`}>
+                        <img src={item.img} alt={item.id} className='w-full h-full mx-auto object-cover' onClick={() => navigate('/dashboard')} />
+                    </div>
+                )
+            }
+        })
+    });
+    useListenToCustomEvent((event) => {
+        if (event.eventName === 'onSlideStartChange') {
+            setCurrentSlide(event?.nextItem?.id)
+        }
+    })
     return (
-        <div className="slider-container">
-            <p className='apps-text-title text-left mb-5 text-3xl font-bold'>Apps</p>
-            <Page
-				effect="coverflow"
-				style={{
-					height: 500,
-					width: 350,
-					overflow: "visible",
-				}}
-			>
-				{images.map((src, index) => (
-                    <motion.div className="slide" key={index} onClick={() => navigate('/dashboard')}>
-                        <img src={src} alt={`Slide ${index}`} />
-                    </motion.div>
-                ))}
-			</Page>
+        <div className="slider-container w-full mb-8">
+            <p className='apps-text-title text-left px-12 mb-6 text-3xl font-bold'>Apps</p>
+            <div className="mx-auto w-9/12 relative">
+                {carouselFragment}
+            </div>
         </div>
     );
 };
