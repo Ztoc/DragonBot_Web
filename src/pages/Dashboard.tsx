6e065cda97df60f0e46eaa91f9ea352559062e78
topdev <maksymkomen0130@gmail.com>
2024-06-04 20:30:04 +0300

    Test bottom menu fix 2.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
     }, [location]);
     return (
         <div className='page-bg !overflow-hidden'>
-            <div className='add-pad flex-1 tap z-[10]'>
+            <div className='add-pad tap z-[10]'>
                 <Score />
                 <CoinImage />
                 <Energy />
