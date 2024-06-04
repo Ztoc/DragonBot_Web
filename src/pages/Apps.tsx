74973d1445c6910b5d2d4790e3822ca9a8bd7ae6
Ztoc <bhu0444@gmail.com>
2024-06-04 23:38:57 +0700

    Bottom menu not showing fix

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -18,20 +18,20 @@ const Apps = () => {
 
 
     return (
-        <div className='page-bg no-xy-move'>
+        <div className='page-bg overflow-hidden'>
             {/*<div className='header-gradient'></div>*/}
             <div className='apps-container h-screen flex flex-col items-center justify-center'>
                 <AppSlider />
                 <div className='apps-buttons-container w-full px-[9%]'>
                     <p className='apps-text-title text-left my-5 text-3xl font-bold'>Join Our Socials</p>
-                    <div className='block flex items-center justify-between'>
+                    <div className='block flex items-center justify-around'>
                         <div className='blur-round-border-bg p-2' style={{
                             '--radius': '13px',
                             '--angle': '135deg',
                         } as React.CSSProperties} onClick={() => {
                             WebApp.openLink(`https://x.com/DragonDotBot`);
                         }}>
-                            <img className='icon-x-img w-[70px] h-[70px]' src={twitterIMG?.src}/>
+                            <img className='icon-x-img w-[60px] h-[60px]' src={twitterIMG?.src}/>
                         </div>
                         <div className='blur-round-border-bg p-2' style={{
                             '--radius': '13px',
@@ -39,7 +39,7 @@ const Apps = () => {
                         } as React.CSSProperties} onClick={() => {
                             WebApp.openLink(`https://medium.com/@dragonbot`);
                         }}>
-                            <img className='icon-m-img w-[70px] h-[70px]' src={mediumImg?.src}/>
+                            <img className='icon-m-img w-[60px] h-[60px]' src={mediumImg?.src}/>
                         </div>
                         <div className='blur-round-border-bg p-2' style={{
                             '--radius': '13px',
@@ -47,7 +47,7 @@ const Apps = () => {
                         } as React.CSSProperties} onClick={() => {
                             WebApp.openTelegramLink(`https://t.me/DragonBotCommunity`);
                         }}>
-                            <img className='icon-t-img w-[70px] h-[70px]' src={telegramImg?.src}/>
+                            <img className='icon-t-img w-[60px] h-[60px]' src={telegramImg?.src}/>
                         </div>
                     </div>
                 </div>
