b98bd19f683545b8154a9c253d2a7b39fca1c7b9
topdev <maksymkomen0130@gmail.com>
2024-06-04 09:56:56 +0300

    Turbo Animation added.

--- src/components/dashboard/Score.tsx
+++ src/components/dashboard/Score.tsx
@@ -2,13 +2,13 @@ import {useSelector} from "react-redux";
 import {AnimatedCounter} from "react-animated-counter";
 import {ImageSliceType} from "../../types/store.ts";
 
-const Score = () => {
+const Score = ({isTurbo = false}) => {
     const score = useSelector((state: any) => state.score.value);
     const image: ImageSliceType = useSelector((state: any) => state.image);
     const COIN_IMG = image.core.find((img) => img.name === 'COIN_TOOL');
     return (
         <div className="flex justify-around items-center animate__animated  animate__fadeIn animate__slow mb-2">
-            <div className='score-holder flex justify-between items-center'>
+            <div className={'score-holder flex justify-between items-center'+ (isTurbo ? ' turbo-counter' : '')}>
                 {COIN_IMG ? <img className='mr-2' src={COIN_IMG?.img.src} alt='DragonCoin'/> : null}
                 <div className='font-extrabold'>
                     <AnimatedCounter
